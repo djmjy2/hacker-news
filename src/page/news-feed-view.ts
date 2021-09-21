cabd3ad92cd13d0151c480768318eab43d5fe5d4
Hwi-Ram <djmjy2@naver.com>
2021-09-21 23:51:16 +0900

    스토어 클래스만들기
    es6로 변경,
    스토어클래스 만들었으나 매개변수 할당에 뭔가 문제가 있다

--- src/page/news-feed-view.ts
+++ src/page/news-feed-view.ts
@@ -1,6 +1,6 @@
 import View from '../core/view';
 import { NewsFeedApi } from '../core/api';
-import { NewsFeed } from '../types';
+import { NewsFeed, NewsStore } from '../types';
 import { NEWS_URL } from '../config';
 
 const template: string = `
@@ -30,26 +30,25 @@ const template: string = `
 
 export default class NewsFeedView extends View {
   private api: NewsFeedApi;
-  private feeds: NewsFeed[];
+  private store: NewsStore;
   
-  constructor(containerId: string) {
+  constructor(containerId: string, store: NewsStore) {
 
     super(containerId, template);
 
     this.api = new NewsFeedApi(NEWS_URL);
-    this.feeds = store.feeds;
+    this.store = store;
   
-    if (this.feeds.length === 0) {
-      this.feeds = store.feeds = this.api.getData();
-      this.makeFeeds();
+    if (!this.store.hasFeeds) {
+      this.store.setFeeds(this.api.getData());
     }
   }
   
-  render(): void {
-    store.currentPage = Number(location.hash.substr(7) || 1);
+  render = (page: string = '1' ): void => {
+    this.store.currentPage = Number(page);
 
-    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
-      const { id, title, comments_count, user, points, time_ago, read } = this.feeds[i];
+    for(let i = (this.store.currentPage - 1) * 10; i < this.store.currentPage * 10; i++) {
+      const { id, title, comments_count, user, points, time_ago, read } = this.store.getFeed(i);
       this.addHtml(`
         <div class="p-6 ${read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
           <div class="flex">
@@ -72,15 +71,11 @@ export default class NewsFeedView extends View {
     }
   
     this.setTemplateData('news_feed', this.getHtml());
-    this.setTemplateData('prev_page', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
-    this.setTemplateData('next_page', String(store.currentPage + 1));
+    this.setTemplateData('prev_page', String(this.store.prevpage));
+    this.setTemplateData('next_page', String(this.store.nextpage));
   
     this.updateView();  
   }
 
-  private makeFeeds(): void {
-    for (let i = 0; i < this.feeds.length; i++) {
-      this.feeds[i].read = false;
-    }
-  }  
+
 }
